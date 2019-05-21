52163bf1398d39a2d54966fcb63d9617b00860d1
Jake Haynes <jakehaynes1023@gmail.com>
2019-05-20 23:40:51 -0400

    Fix bug with window resizing when dragging in maximized state

---
+++ src/sudoku/core/SudokuMain.java
@@ -0,0 +1,85 @@
+package sudoku.core;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import sudoku.factories.LayoutFactory;
+import sudoku.factories.MenuFactory;
+import sudoku.view.MainApplicationView;
+import sudoku.view.RootStackPane;
+import sudoku.view.menu.ApplicationTitleBar;
+import sudoku.view.util.ResourceConstants;
+import sudoku.view.util.WindowHelper;
+
+/** Entry point for the execution of the project. */
+public class SudokuMain extends Application {
+
+	private static final Logger LOG = LogManager.getLogger(SudokuMain.class);
+
+	@Override
+	public void start(final Stage stage) throws IOException {
+		final BorderPane root = this.createRootPane(stage);
+		final RootStackPane rootStackPane = LayoutFactory.getInstance()
+				.createRootStackPane(root);
+		final Scene scene = this.createScene(rootStackPane);
+		this.configureStage(stage, scene, root);
+		// These messages are just to separate executions if a log file gets re-used.
+		LOG.info("==============================================");
+		LOG.info("Application started successfully.");
+	}
+
+	/**
+	 * Creates and returns a Scene, using the given Parent object as a root element.
+	 */
+	private Scene createScene(final Region root) {
+		final Scene scene = new Scene(root);
+		final String applicationCSS = this.getClass()
+				.getResource(ResourceConstants.APPLICATION_CSS).toExternalForm();
+		scene.getStylesheets().add(applicationCSS);
+		scene.setFill(Color.TRANSPARENT);
+		return scene;
+	}
+
+	/**
+	 * Creates and returns a BorderPane which acts as the root element for the
+	 * scene.
+	 */
+	private BorderPane createRootPane(final Stage stage) {
+		final BorderPane root = new BorderPane();
+		final ApplicationTitleBar menuContainer = MenuFactory.getInstance()
+				.createApplicationTitleBar(stage);
+		root.setTop(menuContainer);
+		final MainApplicationView topLevelSplitPane = LayoutFactory.getInstance()
+				.createMainApplicationView();
+		root.setCenter(topLevelSplitPane);
+		return root;
+	}
+
+	private void configureStage(final Stage stage, final Scene scene, Region root) {
+		ViewController.getInstance().setStage(stage);
+		stage.getIcons().add(new Image(this.getClass()
+				.getResourceAsStream(ResourceConstants.APPLICATION_ICON)));
+		stage.initStyle(StageStyle.TRANSPARENT);
+		stage.setScene(scene);
+		stage.setMinHeight(600);
+		stage.setMaximized(true);
+		stage.show();
+		WindowHelper.addResizeAndDragListener(stage, root);
+		ModelController.getInstance();
+	}
+
+	public static void main(final String[] args) {
+		launch(args);
+	}
+
+}
--- src/sudoku/state/window/SoftRestoredState.java
+++ src/sudoku/state/window/SoftRestoredState.java
@@ -1,5 +1,9 @@
 package sudoku.state.window;
 
+import javafx.collections.ObservableList;
+import javafx.geometry.BoundingBox;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import sudoku.core.ViewController;
 import sudoku.view.util.ResourceConstants;
@@ -18,7 +22,15 @@ public class SoftRestoredState extends ApplicationWindowState {
 	protected void onEnter() {
 		final Stage stage = ViewController.getInstance().getStage();
 		stage.setMaximized(false);
-		this.restoreSavedSize(stage);
+		final ObservableList<Screen> screensForRectangle = Screen.getScreensForRectangle(stage.getX(), stage.getY(),
+				stage.getWidth(), stage.getHeight());
+		final Screen screen = screensForRectangle.get(0);
+		final Rectangle2D visualBounds = screen.getVisualBounds();
+		this.savedBounds = new BoundingBox(stage.getX(), stage.getY(), stage.getWidth(), stage.getHeight());
+		stage.setX(visualBounds.getMinX());
+		stage.setY(visualBounds.getMinY());
+		stage.setWidth(visualBounds.getWidth());
+		stage.setHeight(visualBounds.getHeight());
 		this.setIcon(ViewController.getInstance().getMaximizeWindowButton(), ResourceConstants.MAXIMIZE_ICON);
 
 	}
